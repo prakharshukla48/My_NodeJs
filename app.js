788cec912bdab2a77aac66cc714bd9fb0e828004
ayush <ayushsahu99@gmail.com>
2019-10-29 16:13:11 +0530

    7: Sent first response with expressjs

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const http = require('http');
 const express = require('express');
 
-const routes = require('./routes');
+// const routes = require('./routes');
 
 const app = express();
+
+app.use((req, res, next) => {
+    console.log('In the middleware!');
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('In another middleware!');  
+    res.send('<h1>Hello from Express!</h1>')  
+});
 
 const server = http.createServer(app);
 
