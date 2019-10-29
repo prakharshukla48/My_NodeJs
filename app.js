4740aca2cd01098798bea6e2a5a08db24ab63dd6
ayush <ayushsahu99@gmail.com>
2019-10-29 17:17:56 +0530

    9: Handling Incoming Requests

--- app.js
+++ app.js
@@ -5,12 +5,11 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware!');
-    next();
-});
+app.use('/add-product', (req, res, next) => {
+    res.send('<h1>Hello from "Add Product"</h1>');
+})
 
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
     console.log('In another middleware!');  
     res.send('<h1>Hello from Express!</h1>')  
 });
