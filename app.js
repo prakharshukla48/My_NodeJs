3dddb3d4d4586e3219f1061b5c2ec1581ca8969f
ayush <ayushsahu99@gmail.com>
2019-10-30 12:01:18 +0530

    11: Limiting Middleware to POST

--- app.js
+++ app.js
@@ -8,17 +8,19 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product', (req, res, next) => {
+app.use('/add-product', (req, res) => {
+    console.log('Hello from add product');
+    
     res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
 });
 
-app.use('/product', (req, res) => {
+app.post('/product', (req, res) => {
     console.log(req.body);
     res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
-    console.log('In another middleware!');  
+    console.log('In another middlewares!');  
     res.send('<h1>Hello from Express!</h1>')  
 });
 
