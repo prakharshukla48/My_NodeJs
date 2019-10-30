7d1f3a18104dd4d08faee0423370a1923bd04d3b
ayush <ayushsahu99@gmail.com>
2019-10-30 12:11:40 +0530

    12: Express Router

---
+++ routes/shop.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.use('/', (req, res, next) => {
+    console.log('In another middlewares!');  
+    res.send('<h1>Hello from Express!</h1>')  
+});
+
+module.exports = router;
