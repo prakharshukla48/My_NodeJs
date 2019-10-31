832300bb741392f1ddecb1d89190983fb134e6e1
ayush <ayushsahu99@gmail.com>
2019-10-31 19:19:10 +0530

    18: Helper Functions

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,14 @@
 const path = require('path');
 const express = require('express');
 
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
 router.get('/add-product', (req, res) => {
     console.log('Hello from add product');
     
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 router.post('/add-product', (req, res) => {
