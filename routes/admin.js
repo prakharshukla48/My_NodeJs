87da5800842cac364f177d0e76ad195907657063
ayush <ayushsahu99@gmail.com>
2019-11-03 18:27:24 +0530

    21: Sharing Data Across Requests and Users

--- routes/admin.js
+++ routes/admin.js
@@ -5,6 +5,8 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
+const products = [];
+
 router.get('/add-product', (req, res) => {
     console.log('Hello from add product');
     
@@ -12,8 +14,9 @@ router.get('/add-product', (req, res) => {
 });
 
 router.post('/add-product', (req, res) => {
-    console.log(req.body);
+    products.push({title: req.body.title});
     res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -4,9 +4,10 @@ const express = require('express');
 const rootDir = require('../util/path');
 
 const router = express.Router();
+const adminData = require('./admin');
 
 router.get('/', (req, res, next) => {
-    console.log('In another middlewares!');  
+    console.log(adminData.products);  
     res.sendFile(path.join(rootDir, 'views', 'shop.html'));  
 });
 
