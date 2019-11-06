91433ef4ebdb479bd3f2698bfb0d254adfb3eae5
ayush <ayushsahu99@gmail.com>
2019-11-06 14:12:01 +0530

    27: Working with Handlebars

--- app.js
+++ app.js
@@ -8,8 +8,10 @@ const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
+const expressHbs = require('express-handlebars');
 
-app.set('view engine', 'pug');
+app.engine('handlebars', expressHbs);
+app.set('view engine', 'handlebars');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({extended: false}));
