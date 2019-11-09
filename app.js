c30b9ab9071f00d211b98e62d8c02e4f856d5527
ayush <ayushsahu99@gmail.com>
2019-11-09 21:23:27 +0530

    32: Finishing the Controllers

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
+const errorController = require('./controllers/error');
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -19,8 +20,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page not found!'});
-});
+app.use(errorController.get404);
 
 app.listen(3000);
