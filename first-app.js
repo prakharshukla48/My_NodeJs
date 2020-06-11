3cfe0b53d5adcc74d0ce048be8576927978383fe
ayush <ayushsahu99@gmail.com>
2020-06-11 19:49:47 +0530

    completed till parsing request bodies

--- first-app.js
+++ first-app.js
@@ -18,6 +18,11 @@ setTimeout(() => {
     });
 }, 2000);
 
+const hobbies = ['Sports', 'Cooking']
+console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
+
+
+
 console.log('Hello!');
 console.log('Hi!')
 
