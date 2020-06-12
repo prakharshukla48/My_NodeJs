452f6b44c3b82c4ff818f60ada24320432370393
ayush <ayushsahu99@gmail.com>
2020-06-12 19:00:15 +0530

    completed express

--- routes/admin.js
+++ routes/admin.js
@@ -2,14 +2,17 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/add-product', (req, res, next) => {
-    res.send('<form action="add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+const path = require('path');
 
+const rootDir = require('../util/path');
+
+router.get('/add-product', (req, res, next) => {
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 router.post('/add-product', (req, res, next) => {
     res.redirect('/');
-    console.log(req.body); 
+     
 });
 
 module.exports = router;
